bc2d208a523bce9884656eb14f137362ef2b46f9
songqy <songqy3@126.com>
2023-12-04 15:04:03 +0800

    update

--- server/index.mjs
+++ server/index.mjs
@@ -10,15 +10,15 @@ import * as build from "../build/index.js";
 const app = express();
 app.use(express.static("public"));
 
+// api routes
 app.use("/api/v1", router);
 
 // and your app is "just a request handler"
 app.all("*", createRequestHandler({ build }));
 
-console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 app.listen(3000, () => {
   if (process.env.NODE_ENV === "development") {
     broadcastDevReady(build);
   }
-  console.log("App listening on http://localhost:3000");
+  console.log("App listening on http://127.0.0.1:3000");
 });
